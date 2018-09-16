36a4b6d6ff2fadb46361ce2c6088ae6bb5da35b0
praneeth <praneeth@LAPTOP-T267KIDL.home>
2018-09-15 22:22:39 -0500

    Purpose: Added login page with session feature, synced login and register functionality, removed unnecessary pages and code.

--- MyApplication/src/domain/login/LoginController.java
+++ MyApplication/src/domain/login/LoginController.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Login
@@ -28,19 +29,21 @@ public class LoginController extends HttpServlet {
 		Login login = new Login(username, pass);
 		Customer c = customerDao.validateCustomer(login);
 		
+		HttpSession session = request.getSession();
+		
 		if(submitType.equals("login") && c!=null && c.getName()!=null){
-			request.setAttribute("message", "Hello "+c.getName());
-			request.getRequestDispatcher("welcome.jsp").forward(request, response);
+		    session.setAttribute("loginStatus", "true");
+			response.sendRedirect(request.getContextPath()+"/index");
 		}else if(submitType.equals("register")){
 			c.setName(request.getParameter("name"));
 			c.setUsername(request.getParameter("username"));
 			c.setPassword(request.getParameter("password"));
 			customerDao.register(c);
-			request.setAttribute("successMessage", "Registration done, please login!");
-			request.getRequestDispatcher("login.jsp").forward(request, response);
+			session.setAttribute("loginStatus", "true");
+			response.sendRedirect(request.getContextPath()+"/index");
 		}else{
-			request.setAttribute("message", "Data Not Found! Please register!");
-			request.getRequestDispatcher("register.jsp").forward(request, response);
+			session.setAttribute("loginStatus", "false");
+			response.sendRedirect(request.getContextPath()+"/index");
 		}
 
 	}

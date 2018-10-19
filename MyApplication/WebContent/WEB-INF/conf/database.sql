1f593e6eeef181e7da3c4cf162c1302f3e0350e7
Diksha <dchhabra1995@gmail.com>
2018-10-18 19:07:36 -0500

    New tables for transaction, and connection of transaction and customer subscription added

--- MyApplication/WebContent/WEB-INF/conf/database.sql
+++ MyApplication/WebContent/WEB-INF/conf/database.sql
@@ -25,11 +25,12 @@ INSERT INTO `customer` (`user_name`,`password`,`full_name`,`email`,`phone`) VALU
 CREATE TABLE `age_group` (
 	`id` int NOT NULL,
 	`name` char(25) NOT NULL UNIQUE,
-	PRIMARY KEY (id)
+	`description` char(100) NOT NULL,
+ 	PRIMARY KEY (id)
 );
 
 -- Dumping data for age group table
-INSERT INTO `age_group` (`id`,`name`) VALUES (1, "New Born"), (2, "Infant"), (3, "Toddler");
+INSERT INTO `age_group` (`id`,`name`,`description`) VALUES (1, "New Born", "0-2 Months"), (2, "Infant", "2 Months - 1 Year"), (3, "Toddler", "1-5 Years");
 
 -- Change auto increment for age group table
 ALTER TABLE `age_group` AUTO_INCREMENT=4;
@@ -46,8 +47,8 @@ CREATE TABLE `subscription` (
 );
 
 -- Dumping data for subscription table
-INSERT INTO `subscription` (`id`,`name`,`age_group`,`created_by`) VALUES (1,"Standard",1,1), (2,"Standard",2,1), (3,"Standard",3,1),
-(4,"Premium",1,1), (5,"Premium",2,1), (6,"Premium",3,1), (7,"Economical",1,1), (8,"Economical",2,1), (9,"Economical",3,1);
+INSERT INTO `subscription` (`id`,`name`,`age_group`,`created_by`) VALUES (1,"Premium",1,1), (2,"Premium",2,1), (3,"Premium",3,1), 
+(4,"Standard",1,1), (5,"Standard",2,1), (6,"Standard",3,1), (7,"Economical",1,1), (8,"Economical",2,1), (9,"Economical",3,1);
 
 -- Change auto increment for subscription table
 ALTER TABLE `age_group` AUTO_INCREMENT=10;
@@ -64,9 +65,12 @@ CREATE TABLE `product` (
 );
 
 -- Dumping data for product table
-INSERT INTO `product` (`id`,`name`,`brand`,`category`,`quantity`,`price`) VALUES (1,"Cerelac Stage-1","Nestle","Instant Cereal","2lbs",17.82),
-(2,"Baby Cereal","Gerber","Instant Cereal","2lbs",12.12),
-(3,"Creamy Porridge","Hipp","Instant Cereal","2lbs",9.20);
+INSERT INTO `product` (`id`,`name`,`brand`,`category`,`quantity`,`price`) VALUES (1,"Cerelac Stage-1","Nestle","Instant Cereal","2lbs",17.50),
+(2,"Baby Cereal","Gerber","Instant Cereal","2lbs",12.00),
+(3,"Creamy Porridge","Hipp","Instant Cereal","2lbs",9.50),
+(4, "Snug N Dry","Huggies","Diapers","20nos",25.00),
+(5, "Swaddlers","Pampers","Diapers","20nos",21.50),
+(6, "Disposable","Luvs","Diapers","20nos",15.50);
 
 -- Product age group mapping table
 CREATE TABLE `product_age_group_mapping` (
@@ -80,7 +84,8 @@ CREATE TABLE `product_age_group_mapping` (
 );
 
 -- Dumping data for product age group mapping table
-INSERT INTO `product_age_group_mapping` (`product_id`,`age_group_id`) VALUES (1,2), (1,3), (2,2), (2,3), (3,2), (3,3);
+INSERT INTO `product_age_group_mapping` (`product_id`,`age_group_id`) VALUES (1,2), (1,3), (2,2), (2,3), (3,2), (3,3), (4,1), (4,2),
+(5,1), (5,2), (6,1), (6,2);
 
 
 -- Subscription product mapping table
@@ -96,7 +101,8 @@ CREATE TABLE `subscription_product_mapping` (
 );
 
 -- Dumping data for subscription product mapping table
-INSERT INTO `subscription_product_mapping` (`subscription_id`,`product_id`,`quantity`) VALUES (2,2,2), (3,2,2), (5,1,2), (6,1,2), (8,3,2), (9,3,2);
+INSERT INTO `subscription_product_mapping` (`subscription_id`,`product_id`,`quantity`) VALUES (5,2,2), (6,2,2), (2,1,2), (3,1,2), (8,3,2), (9,3,2),
+(4,5,4), (5,5,4), (1,4,4), (2,4,4), (7,6,4), (8,6,4);
 
 -- Customer subscription mapping table
 CREATE TABLE `customer_subscription_mapping` (
@@ -105,10 +111,34 @@ CREATE TABLE `customer_subscription_mapping` (
 	`subscription_id` int NOT NULL,
 	`frequency` char(25) NOT NULL,
 	`quantity` int NOT NULL,
+	`duration` int NOT NULL,
+	`start_date` timestamp NOT NULL,
+	`status`, boolean NOT NULL DEFAULT 1, 
 	PRIMARY KEY (id),
 	CONSTRAINT fk_customer_subscription_mapping_customer_id FOREIGN KEY (`customer_id`) REFERENCES `customer`(`id`),
 	CONSTRAINT fk_customer_subscription_mapping_subscription_id FOREIGN KEY (`subscription_id`) REFERENCES `subscription`(`id`),
-	CONSTRAINT ck_customer_subscription_mapping_frequency CHECK (`frequency` IN ("weekly","bi-weekly","monthly"))
+	CONSTRAINT ck_customer_subscription_mapping_frequency CHECK (`frequency` IN ("weekly","bi-weekly","monthly")),
+	CONSTRAINT ck_customer_subscription_mapping_duration CHECK (`duration` IN (3,6,9,12))
+);
+
+CREATE TABLE `transaction` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`transaction_date` timestamp NOT NULL,
+	`mode` char(100) NOT NULL,
+	`address` char(200) NOT NULL,
+	`amount` float(10,2) NOT NULL,
+	`card_no` INT,
+	PRIMARY KEY(id)
+);
+
+
+CREATE TABLE `transaction_customer_subscription_mapping` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`transaction_id` int NOT NULL,
+	`customer_subscription_id` int NOT NULL,
+	PRIMARY KEY(id),
+	CONSTRAINT fk_transaction_customer_subscription_mapping_customer_subscription_id FOREIGN KEY (`customer_subscription_id`) REFERENCES `customer_subscription_mapping`(`id`),
+	CONSTRAINT fk_transaction_customer_subscription_mapping_transaction_id FOREIGN KEY (`transaction_id`) REFERENCES `transaction`(`id`)
 );
 
 

f540dce93b1929d6f10f5f7f79b438ccc4750572
kpasagada <k.pasagada@gmail.com>
2018-10-28 22:10:57 -0500

    Purpose: Registration tests suite class.

---
+++ MyApplication/src/domain/login/test/RegisterCustomerAllTests.java
@@ -0,0 +1,14 @@
+package domain.login.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   RegisterCustomerUITest.class,
+   RegisterCustomerTest.class
+})
+public class RegisterCustomerAllTests {
+
+}
